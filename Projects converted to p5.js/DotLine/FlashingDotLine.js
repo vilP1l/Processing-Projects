1909f92a908f9091dc1c8d9fa7dcb0f6bdf7d084
Vilius <vilius3456@gmail.com>
2017-11-25 19:21:54 -0500

    Added FlashingDotLine

---
+++ Projects converted to p5.js/DotLine/FlashingDotLine.js
@@ -0,0 +1,40 @@
+var num;
+var dot1;
+function setup()
+{
+  num = 200;
+  createCanvas(screen.width,screen.height);
+}
+function draw()
+{
+  background(random(255),random(255),random(255));
+  for(var n = 0; n < num; n++)
+  {
+    dot1 = new Dot
+    dot1.render();
+    dot1.calc();
+  }
+}
+
+
+
+function Dot()
+{  
+    this.x = random(width);
+    this.y = random(height);
+
+  this.render = function render()
+  {
+    ellipse(this.x, this.y, 5, 5);  
+  }
+ this.calc = function calc()
+  {
+    var d = dist(this.x,this.y,mouseX,mouseY);
+    if(d < 10000)
+    {
+      stroke(random(255),random(255),random(255));
+      strokeWeight(2);
+      line(this.x,this.y,mouseX,mouseY);
+    }
+  }
+}
